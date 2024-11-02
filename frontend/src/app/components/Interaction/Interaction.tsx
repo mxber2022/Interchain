bc93f0d0a336cd58493e0c42c37ceef17fa53b63
mxber2022 <mxber2022@gmail.com>
2024-11-02 20:44:52 +0700

    klaster integration

--- frontend/src/app/components/Interaction/Interaction.tsx
+++ frontend/src/app/components/Interaction/Interaction.tsx
@@ -3,7 +3,69 @@
 import React, { useState } from "react";
 import { ethers } from "ethers";
 import { fetchABI, getContractInstance } from "@/utils/contract";
-import styles from "./Interaction.module.css";
+import styles from "./Interaction.module.css"; // Ensure you have a CSS module for styling
+
+// async function setupKluster() {
+//     const account = privateKeyToAccount(
+//       "0x230d41177fafa38ae07d56401f78a9d86e546eb5d3874487e18296344f7a725f"
+//     );
+
+//     const klaster = await initKlaster({
+//       accountInitData: loadBiconomyV2Account({
+//         owner: account.address,
+//       }),
+//       nodeUrl: klasterNodeHost.default,
+//     });
+
+//     console.log(klaster.account.uniqueAddresses);
+
+//     const mcClient = buildMultichainReadonlyClient(
+//       [
+//         optimism,
+//         base,
+//         polygon,
+//         arbitrum,
+//         scroll,
+//         optimismSepolia,
+//         arbitrumSepolia,
+//       ].map((x) => {
+//         return {
+//           chainId: x.id,
+//           rpcUrl: x.rpcUrls.default.http[0],
+//         };
+//       })
+//     );
+
+//     const mcUSDC = buildTokenMapping([
+//       deployment(optimism.id, "0x0b2c639c533813f4aa9d7837caf62653d097ff85"),
+//       deployment(arbitrum.id, "0xaf88d065e77c8cC2239327C5EDb3A432268e5831"),
+//     ]);
+
+//     const intersectTokenAndClients = (
+//       token: MultichainTokenMapping,
+//       mcClient: MultichainClient
+//     ) => {
+//       return token.filter((deployment) =>
+//         mcClient.chainsRpcInfo
+//           .map((info) => info.chainId)
+//           .includes(deployment.chainId)
+//       );
+//     };
+
+//     // Store the intersection of the Klaster provided token and the chains your project is using.
+//     const mUSDC = intersectTokenAndClients(mcUSDC, mcClient);
+//     const uBalance = await mcClient.getUnifiedErc20Balance({
+//       tokenMapping: mUSDC,
+//       account: klaster.account,
+//     });
+
+//     console.log(uBalance.balance);
+//     console.log(uBalance.breakdown); // Breakdown of balances across each separate blockchain
+
+//     // The decimals of the token. In order for tokenMapping to be created,
+//     // all instances must have the same number of decimals.
+//     // uBalance.decimals;
+//   }
 
 const Interaction: React.FC = () => {
   const [contractAddress, setContractAddress] = useState<string>("");
@@ -33,6 +95,7 @@ const Interaction: React.FC = () => {
     }
 
     try {
+      // Fetch ABI using the chain's API endpoint and contract address
       const abi = await fetchABI(
         selectedChainData.api,
         contractAddress,
@@ -41,6 +104,8 @@ const Interaction: React.FC = () => {
       setAbi(abi);
       setError(""); // Clear any previous errors
 
+      // Initialize ethers provider and contract instance
+      //@ts-ignore
       const web3Provider = new ethers.providers.Web3Provider(window.ethereum);
       await web3Provider.send("eth_requestAccounts", []);
       const signer = web3Provider.getSigner();
@@ -80,8 +145,18 @@ const Interaction: React.FC = () => {
       (input: any) => inputValues[functionName]?.[input.name] || ""
     );
 
+    // Check if the function is payable and handle the value accordingly
+    const isPayable = inputs.some((input: any) => input.type === "payable");
+    console.log("isPayable: ", isPayable);
+
     try {
-      const tx = await contract[functionName](...params);
+      const tx = isPayable
+        ? await contract[functionName](...params, {
+            value: ethers.utils.parseEther(
+              inputValues[functionName]?.value || "0"
+            ),
+          })
+        : await contract[functionName](...params);
       await tx.wait();
       console.log("Transaction successful", tx);
     } catch (err) {
@@ -90,8 +165,8 @@ const Interaction: React.FC = () => {
   };
 
   return (
-    <div>
-      <h1>NFT Contract Interaction</h1>
+    <div className={styles.container}>
+      <h1 className={styles.title}>NFT Contract Interaction</h1>
       <input
         className={styles.inputElement}
         type="text"
@@ -100,7 +175,7 @@ const Interaction: React.FC = () => {
         onChange={(e) => setContractAddress(e.target.value)}
       />
       <select
-        className={styles.inputElement}
+        className={styles.selectElement}
         onChange={(e) => setSelectedChain(e.target.value)}
         value={selectedChain}
       >
@@ -114,17 +189,19 @@ const Interaction: React.FC = () => {
         Fetch ABI
       </button>
 
-      {error && <p className={styles.error}>{error}</p>}
+      {error && <p style={{ color: "red" }}>{error}</p>}
 
       {abi.length > 0 && (
         <div>
           <h2>Functions</h2>
           {abi.map((item: any) =>
             item.type === "function" &&
-            item.stateMutability === "nonpayable" ? (
+            (item.stateMutability === "nonpayable" ||
+              item.stateMutability === "payable") ? (
               <div key={item.name} className={styles.functionBlock}>
                 <h3>{item.name}</h3>
 
+                {/* Dynamically render input fields for function arguments */}
                 {item.inputs.length > 0 && (
                   <div>
                     {item.inputs.map((input: any) => (
@@ -142,6 +219,18 @@ const Interaction: React.FC = () => {
                         }
                       />
                     ))}
+
+                    {/* Input for value if the function is payable */}
+                    {item.stateMutability === "payable" && (
+                      <input
+                        className={styles.inputElement}
+                        type="text"
+                        placeholder="Value in ETH"
+                        onChange={(e) =>
+                          handleInputChange(item.name, "value", e.target.value)
+                        }
+                      />
+                    )}
                   </div>
                 )}
 
